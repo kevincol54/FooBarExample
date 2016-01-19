efd99a2622694460463136ee7575b061250c8789
Kevin <kevincol54@gmail.com>
2016-01-19 18:10:53 -0500

    initial commit

--- index.js
+++ index.js
@@ -5,13 +5,9 @@
 var sum = 0
 //loop through numbers less than 1000
 for(var i = 0; i < 1000; i++) {
-  //check if the remainder is 0 when divided by 3
-  if (i % 3 === 0) {
-    //if the remainder is zero when divided by 3, check if the remainder is 0 when divided by 5
-    if (i % 5 === 0) {
-      //if the number passes both the above checks, then reassign the variable sum to equal sum plus the number that passed the checks
-      sum = sum + i
-    }
+  //check if the remainder is 0 when divided by 3 or 5
+  if ((i % 3 === 0) || (i % 5 ===0)) {
+      sum += i
   }
 }
 console.log("sum:", sum)
