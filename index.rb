69a5c5b3c10c73880ae21f5e2ffd33dcd2c4316e
Kevin <kevincol54@gmail.com>
2016-01-19 20:23:37 -0500

    ruby example

---
+++ index.rb
@@ -0,0 +1,13 @@
+# find sum of all numbers that are divisable by 3 or 5 that are below 1000
+# to run this, CD into the directory and type the command 'ruby index.rb'
+
+# start of by setting the sum variable to 0
+sum = 0
+# loop through numbers less than 1000
+for i in 0..999
+  # check if the remainder is 0 when divided by 3 or 5
+  if i % 3 === 0 || i % 5 === 0
+    sum+=i
+  end
+end
+puts sum
